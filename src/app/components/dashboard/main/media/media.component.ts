24021892305179164bf8d813e91e833013b9003f
AhmedAbdelazez27 <ahmed.abdelazeez@jrm.com.eg>
2024-12-30 23:09:21 +0200

    add media center at nav and add the slider media

--- src/app/components/dashboard/main/media/media.component.ts
+++ src/app/components/dashboard/main/media/media.component.ts
@@ -4,20 +4,21 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { AboutusService } from '../servicesApi/aboutus.service';
 import { SpinnerService } from '../../../../shared/services/spinner.service';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-media',
   standalone: true,
-  imports: [TranslateModule,CommonModule],
+  imports: [TranslateModule,CommonModule,FormsModule],
   templateUrl: './media.component.html',
   styleUrl: './media.component.scss'
 })
 export class MediaComponent implements OnInit{
-  currentLang: string;
-  itemMediaCenter: any[]=[];
-
+  currentIndex:any;
+  itemMediaCenter: any[] = [];
   itemMediaCenterF: any[] = [];
   selectedMedia: any = null;
+  currentLang: string = 'en'; // or 'ar', based on your logic
 
   constructor(
     private aboutService: AboutusService,
@@ -30,28 +31,41 @@ export class MediaComponent implements OnInit{
   ngOnInit(): void {
     this.fetchingMediaCenterData();
   }
-
-  fetchingMediaCenterData(){
+  fetchingMediaCenterData() {
     this._SpinnerService.showSpinner();
     this.aboutService.getmediaCenter().subscribe({
-      next:(res)=>{
+      next: (res) => {
         this._SpinnerService.hideSpinner();
-        console.log(res);
         this.itemMediaCenter = res.result;
 
-        this.itemMediaCenterF = res.result.map((item:any) => ({
+        this.itemMediaCenterF = this.itemMediaCenter.map((item: any) => ({
           ...item,
           title: this.currentLang === 'ar' ? item.titleAr : item.titleEn
         }));
-        if (this.itemMediaCenter.length > 0) {
-          this.selectedMedia = this.itemMediaCenterF[2]; // Default selection
+
+        if (this.itemMediaCenterF.length > 0) {
+          this.selectedMedia = this.itemMediaCenterF[this.currentIndex]; // Set the first item as the default
         }
       },
-      error:(err)=>{
-        console.log(err);
+      error: (err) => {
+        console.error(err);
         this._SpinnerService.hideSpinner();
       }
-    })
+    });
+  }
+
+  onSlide(event: any) {
+    // Access the current index from the event object
+    const activeIndex = Array.from(event.target.querySelectorAll('.carousel-item')).findIndex((item: any) =>
+      item.classList.contains('active')
+    );
+    this.currentIndex = activeIndex;
+    this.selectedMedia = this.itemMediaCenterF[this.currentIndex];
+    console.log('Current Index:', this.currentIndex);
+  }
+
+  selectedItem(i:number){
+    this.currentIndex = i;
   }
 
 }
